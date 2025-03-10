7051cd1678dd056b30ed57d4be87f2e168c89c68
sricharan8462 <sricharan8462@yahoo.com>
2025-03-09 21:21:41 -0400

    Add Plan Model and Calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +18,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
+enum Priority { low, medium, high }
+
+class Plan {
+  String name;
+  String description;
+  DateTime date;
+  bool isCompleted;
+  Priority priority;
+
+  Plan({
+    required this.name,
+    required this.description,
+    required this.date,
+    this.isCompleted = false,
+    this.priority = Priority.low,
+  });
+}
+
 class PlanManagerScreen extends StatefulWidget {
   const PlanManagerScreen({super.key});
 
@@ -25,11 +44,33 @@ class PlanManagerScreen extends StatefulWidget {
 }
 
 class _PlanManagerScreenState extends State<PlanManagerScreen> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Adoption & Travel Planner')),
-      body: const Center(child: Text('Plan Manager')),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2020, 1, 1),
+            lastDay: DateTime.utc(2030, 12, 31),
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            calendarStyle: const CalendarStyle(
+              todayDecoration: BoxDecoration(color: Colors.blueAccent, shape: BoxShape.circle),
+              selectedDecoration: BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
